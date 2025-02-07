bf678dc9f6f63f46f4fe0d4740f1bc4f2c6afe98
Nika-yatashvili <nika.katashvili2@gmail.com>
2025-02-07 05:41:54 -0800

    assignment3

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@
 
 // task 1 
 
-for(let i = 1; i<=10; i ++) {
-    console.log(i)
+for(let i = 1; i<=10; i ++); {
+    console.log(i);
 }
 
 // task2 
-for(let x = 10; x>=1; x--) {
-    console.log(x)
+for(let x = 10; x>=1; x--); {
+    console.log(x);
 }
 
 // task 3 
 // 50 is yvela mteli ricxvis jamia sum_i
 // when i=2 then sum=3; i=3 ; sum = 6 ; i=4 ; sum= 10...
 let sum = 0;
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 50; i++); {
     sum += i;
 }
 console.log(sum);
@@ -23,23 +23,23 @@ console.log(sum);
 // task 4 
 
 // 5 iteracia: 0;2;4;6;8
-for (let i = 0; i < 10; i += 2) {
+for (let i = 0; i < 10; i += 2); {
     console.log(i);
 }
 
 // task7 
 for(let z = 1; z<=100; z++) {
-    if(z>27)
-        break
-    console.log(z)
+    if(z>27);
+        break;
+    console.log(z);
 }
 
 
 // task10 
-console.log("all even numbers task 10")
-for(let q = 1; q<=1000; q++) {
-    if(q % 2 == 0) {
-        console.log(q)
+console.log("all even numbers task 10");
+for(let q = 1; q<=1000; q++); {
+    if(q % 2 == 0); {
+        console.log(q);
 
     }
 
